9b6dbefbbcdef2423eb9ee4854be811c20ceb6c4
wliang <wliang@tsari.tsinghua.edu.cn>
2021-01-04 16:33:03 +0800

    NEW: init verision
    Signed-off-by: wliang <wliang@tsari.tsinghua.edu.cn>

---
+++ cpp_sample/recev_detection.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <memory>
+
+#include "UDPServer.h"
+#include "detection.pb.h"
+
+int main(int argc, char **argv) {
+    std::cout << "receive detection results" << std::endl;
+    std::unique_ptr<UDPServer> udp_server(new UDPServer(9000));
+    while (true) {
+        char recv_buf[10240] = "";
+        int receveSize = udp_server->UDPServerReceive(recv_buf, 10240);
+        std::cout << "received " << receveSize << " bytes" << std::endl;
+        Detection det;
+        bool isValid = det.ParseFromArray(recv_buf, receveSize);
+        if (isValid) {
+            std::cout << det.DebugString() << std::endl;
+        }
+    }
+}
---
+++ python3_sample/rece_detection.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import socket
+
+sys.path.append(os.getcwd())
+
+from proto import detection_pb2
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip_port = ('127.0.0.1', 9000)
+    server.bind(ip_port)
+    while True:
+        data, peer = server.recvfrom(10240)
+        det = detection_pb2.Detection()
+        det.ParseFromString(data)
+        print(det)
+
+if __name__ == '__main__':
+    main()
