26108104a14b42fb2f08c7f2d70e7bc3f43eb63d
wliang <wliang@tsari.tsinghua.edu.cn>
2021-03-25 08:58:12 +0800

    ENH: update new interface protocol
    1. add freespace parse sample
    
    Signed-off-by: wliang <wliang@tsari.tsinghua.edu.cn>

--- cpp_sample/recev_detection_udp.cpp
+++ cpp_sample/recev_detection_udp.cpp
@@ -8,12 +8,16 @@ int main(int argc, char **argv) {
     std::cout << "UDP receive detection results" << std::endl;
     std::unique_ptr<UDPServer> udp_server(new UDPServer(9000));
     while (true) {
-        char recv_buf[26000] = "";
-        int receveSize = udp_server->UDPServerReceive(recv_buf, 26000);
+        char recv_buf[65536] = "";
+        int receveSize = udp_server->UDPServerReceive(recv_buf, 65536);
         std::cout << "received " << receveSize << " bytes" << std::endl;
         Detection det;
         bool isValid = det.ParseFromArray(recv_buf, receveSize);
         if (isValid) {
+            Freespace freespace;
+            freespace.ParseFromString(det.freespace());
+            det.clear_freespace();
+            // std::cout << freespace.DebugString() << std::endl;
             std::cout << det.DebugString() << std::endl;
         }
     }
--- python3_sample/recev_detection.py
+++ python3_sample/recev_detection.py
@@ -11,9 +11,12 @@ def main():
     ip_port = ('127.0.0.1', 9000)
     server.bind(ip_port)
     while True:
-        data, peer = server.recvfrom(26000)
+        data, peer = server.recvfrom(65536)
         det = detection_pb2.Detection()
         det.ParseFromString(data)
+        freespace = detection_pb2.Freespace()
+        freespace.ParseFromString(det.freespace)
+        det.ClearField('freespace')
         print(det)
 
 if __name__ == '__main__':
